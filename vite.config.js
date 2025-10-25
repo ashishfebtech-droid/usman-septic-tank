9f9026c4322498cc5f22b278d2203318217466ca
Ashish kumar <ak85522114@gmail.com>
2025-10-25 18:32:12 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/usman-septic-tank/',
+  base: '/usman-septic-tank/',  // Yehi use karein
 })
