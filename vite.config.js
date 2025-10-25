099141f14891a7303544b73330682367f1e3550d
Ashish kumar <ak85522114@gmail.com>
2025-10-25 18:24:46 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/usman-septic-tank/'  // Comment out for local development
+  base: process.env.NODE_ENV === 'production' ? '/usman-septic-tank/' : '/',
 })
