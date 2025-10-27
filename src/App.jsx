4fe65f7f565dcad5eb2d87141575816901c2242b
Ashish kumar <ak85522114@gmail.com>
2025-10-27 10:47:46 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import FloatingButtons from './components/FloatingButtons';
-import ScrollToTop from './components/ScrollToTop';
-import Home from './pages/Home';
-import Gallery from './pages/Gallery';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import FloatingButtons from "./components/FloatingButtons";
+import ScrollToTop from "./components/ScrollToTop";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import { useLenis } from "./hooks/useLenis";  // Add this import
+import "lenis/dist/lenis.css";  // Add this import
 
 function App() {
+  useLenis();
+  
   return (
-    <Router basename="/usman-septic-tank">  {/* Add basename here */}
+    <Router basename="/usman-septic-tank">
       <div className="App">
         <Header />
         <main>
